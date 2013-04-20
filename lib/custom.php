26be3cdb3c4fcc0e4b626a3ef66fe96bc4b407b2
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2013-04-19 21:31:52 -0700

    format to WP standards

--- lib/custom.php
+++ lib/custom.php
@@ -10,16 +10,16 @@
  * Toolbar modifications and tweaks
  */
 function toolbar_tweaks() {
-    global $wp_admin_bar;
+	global $wp_admin_bar;
 
-    $wp_admin_bar->remove_menu('wp-logo');
-    $wp_admin_bar->remove_menu('about');
-    $wp_admin_bar->remove_menu('wporg');
-    $wp_admin_bar->remove_menu('documentation');
-    $wp_admin_bar->remove_menu('support-forums');
-    $wp_admin_bar->remove_menu('feedback');
-    $wp_admin_bar->remove_menu('view-site');
-    $wp_admin_bar->remove_menu('site-name');
+	$wp_admin_bar->remove_menu('wp-logo');
+	$wp_admin_bar->remove_menu('about');
+	$wp_admin_bar->remove_menu('wporg');
+	$wp_admin_bar->remove_menu('documentation');
+	$wp_admin_bar->remove_menu('support-forums');
+	$wp_admin_bar->remove_menu('feedback');
+	$wp_admin_bar->remove_menu('view-site');
+	$wp_admin_bar->remove_menu('site-name');
 }
 
 add_action( 'wp_before_admin_bar_render', 'toolbar_tweaks' );
