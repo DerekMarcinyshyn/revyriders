c6b22f40a4c0975d23ab32b3383c3c015b0f7c84
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2013-05-18 20:30:42 -0700

    add maintenance 503

---
+++ 503.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Revy Riders - Maintenance Mode</title>
+		<style>
+			html, body {margin:0;padding:0;background: #333;color:#eee;font-family:Arial,sans-serif;}
+			.container {width:100%;}
+			.row {width:420px;margin:0 auto;text-align: center;}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<h1>Revy Riders</h1>
+				<p>We are currently upgrading our website.</p>
+				<p>Be back shortly.</p>
+			</div>
+		</div>
+	</body>
+</html>
